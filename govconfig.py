4fb18dc27f884687d2cf3e0e3236478f79e8c302
Linga K <59007268+LKSource@users.noreply.github.com>
2024-06-19 17:47:35 +0530

    Create govconfig.py

---
+++ govconfig.py
@@ -0,0 +1,16 @@
+segments = {'PBSvsAlcon': {}, 'Demand': {}, 'Milestones': {}}
+segments['PBSvsAlcon']["subject"] = "Comcast - PBs-Alcon volume"
+segments['PBSvsAlcon']["message"] = "Pls fix the Alcon vs PBS gap for following projects on priority"
+segments['PBSvsAlcon']["to"] = ""
+segments['PBSvsAlcon']["cc"] = ""
+
+segments['Demand']["subject"] = "CBL1 Offshore Demand Report "
+segments['Demand']["message"] = "Cable One Offshore Demand Report ! <br> Total Offshore Demands : Cnt"
+segments['Demand']["to"] = ""
+segments['Demand']["cc"] = ""
+
+segments['Milestones']["subject"] = "Comcast - Unbilled milestones"
+segments['Milestones']["message"] = ("Pls raise confirmation for the following unbilled milestones. Share the reason "
+                                     "if it can't be raised")
+segments['Milestones']["to"] = ""
+segments['Milestones']["cc"] = ""
